44509aed85a79b80cf7c0c9c81dd83ce78b16de8
ergosteur <matt.yiptong@gmail.com>
2018-01-01 21:10:09 -0500

    wip add json output to backend

--- paginate.php
+++ paginate.php
@@ -1,6 +1,7 @@
 <?php
 $dir = 'images/';
 $maxi = 2500;
+# parse GET arguments
 if (empty($_GET)) {
 	$page = 0;
 	$pageSize = 25;
@@ -8,25 +9,48 @@ if (empty($_GET)) {
 else {
 	$page = $_GET['page'];
 	$pageSize = $_GET['pagesize'];
+	$json = $_GET['json'];
 	}
-
-$files = scandir($dir);
+# open finfo to get mime types
 $finfo = new finfo();
+# get files in specified directory
+$files = preg_grep('/^([^.])/', scandir($dir)); #preg_grep to remove dotfiles https://stackoverflow.com/questions/8532569/exclude-hidden-files-from-scandir
+# inject directory/path into files array
+foreach ($files as &$value) {
+    $value = $dir.$value;
+}
+unset($value);
+
+# break up array by chunks according to arguments
 $pagedFiles = array_chunk($files, $pageSize, true);
 $i = 0;
+$j = 0;
+
+# create pagedFilesObj for json output
+$pagedFilesObj->nPages = sizeof($pagedFiles);
+$pagedFilesObj->data = $pagedFiles;
+
 
 //print_r($files);
-?>
-<?php //echo json_encode($pagedFiles[$page]);
-foreach ($pagedFiles[$page] as &$filename) {
-	$i++;
-	$fileMimeType = $finfo->file($dir . $filename, FILEINFO_MIME_TYPE);
-	if (strpos($fileMimeType, "image") !== false AND $i <= $maxi) {
-		echo '<a class="swipeboxImg" href="' . $dir . $filename . '"><img alt="'. $filename . '" src="' . $dir . $filename . '" /></a>';
-		}
-	elseif ($i > $maxi) {
-		break;
-		}
-	#print_r($filename . " " . $fileMimeType . "\n");
-	} 
+
+if ($json == 1 AND $page == 'all') {
+	echo json_encode($pagedFilesObj);
+}
+elseif ($json == 1 AND !is_null($page)) {
+	echo json_encode($pagedFiles[$page]);
+}
+elseif ($json == 0 OR is_null($json)) {
+	foreach ($pagedFiles[$page] as &$filename) {
+		$i++;
+		$fileMimeType = $finfo->file($filename, FILEINFO_MIME_TYPE);
+		if (strpos($fileMimeType, "image") !== false AND $i <= $maxi) {
+			echo '<a class="swipeboxImg" href="' . $filename . '"><img alt="'. str_replace($dir,"",$filename) . '" src="' . $filename . '" /></a>'; #remove dir from alt-text
+			}
+			elseif ($i > $maxi) {
+				break;
+			}
+			#print_r($filename . " " . $fileMimeType . "\n");
+	}
+}
+else { echo "nomatch";}
 ?>
